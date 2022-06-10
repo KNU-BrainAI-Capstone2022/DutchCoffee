8af7d13a9de7812e4b20e92aff245fea7c3ff733
chado28 <chado28@naver.com>
2022-06-10 21:03:09 +0900

    asdf

--- data.py
+++ data.py
@@ -10,34 +10,15 @@ import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 import os
 import random
- 
 import requests
 import time
 from tqdm import tqdm
+import math
 
-
-def seed_everything(seed: int = 42):
-    random.seed(seed)
-    np.random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)  # type: ignore
-    torch.backends.cudnn.deterministic = True  # type: ignore
-    torch.backends.cudnn.benchmark = True  # type: ignore
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-torch.backends.cudnn.benchmark = True
-
-
-
+#%% ABOUT API
 result = requests.get('https://api.binance.com/api/v3/ticker/price')
 js = result.json()
-#symbols = [x['symbol']for x in js]
 symbols = 'BTCUSDT'
-#symbols_usdt = [x for x in symbols if 'USDT' in x]
-
-#COLUMNS = ['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close_time', 'quote_av', 'trades', 
-#                   'tb_base_av', 'tb_quote_av', 'ignore']
 COLUMNS = ['Open_time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close_time', 'quote_av', 'trades', 
                    'tb_base_av', 'tb_quote_av', 'ignore']
 
@@ -82,47 +63,29 @@ def get_data(start_date, end_date, symbol):
 #start_date = '2022-03-01'
 #end_date = '2022-03-20'
 start_date = '2022-01-01'
-end_date = '2022-05-15'
+end_date = '2022-05-01'
 symbol = 'BTCUSDT'
-df2 = get_data(start_date, end_date, symbol)
+df2 = get_data(start_date, end_date, symbol) 
+former = []
+later = []
+newdata = pd.DataFrame(columns=['Price','Label','Mean','Stddev','Z','ZL'])
 
-train2 = []
-newdata = pd.DataFrame(columns=['Price'])
 
-
-for i in range(int((df2['Close'].size)/30 -1 )):
+for i in range(int((df2['Close'].size)/10 -7 )):
     print(i)
     temp = []
+    temp2 = []
     for k in range(60):
-        temp.append(int(float(df2['Close'][i*30+k])))
-    train2.append(temp)
+        temp.append(float(df2['Close'][i*10+k]))
+    for k in range(10):
+        temp2.append(float(df2['Close'][i*10+k+60]))
+    former.append(temp)
+    later.append(temp2)
 
-for i in range(len(train2)):
-    newdata.loc[i,'Price'] = train2[i]
-    
-newdata.to_pickle('pretrain.pkl')
-#%% z 표준화
-#%% 첫 번째 프리트레인
-#start_date = '2022-03-01'
-#end_date = '2022-03-20'
-start_date = '2021-05-15'
-end_date = '2022-05-15'
-symbol = 'BTCUSDT'
-df2 = get_data(start_date, end_date, symbol)
-
-train2 = []
-newdata = pd.DataFrame(columns=['Price','Mean','Stddev','Z'])
-
-
-for i in range(int((df2['Close'].size)/30 -1 )):
-    print(i)
-    temp = []
-    for k in range(60):
-        temp.append(int(float(df2['Close'][i*30+k])))
-    train2.append(temp)
-
-for i in range(len(train2)):
-    newdata.loc[i,'Price'] = train2[i]
+for i in range(len(former)):
+    newdata.loc[i,'Price'] = former[i]
+for i in range(len(later)):
+    newdata.loc[i,'Label'] = later[i]
     
 meanlist = []
 for i in range(len(newdata['Price'])):
@@ -132,59 +95,27 @@ for i in range(len(newdata['Price'])):
     mean = mean/len(newdata['Price'][0])
     newdata.loc[i,'Mean'] = mean
     z = []
+    zl = []
     stddev = 0
     for j in newdata['Price'][i]:
         stddev += (j-mean)**2
     stddev = stddev/len(newdata['Price'][0])
+    stddev = math.sqrt(stddev)
     newdata.loc[i,'Stddev'] = stddev
     for j in newdata['Price'][i]:
-        z.append(int((round((j-mean)/stddev,3))*1000+1000))
+        z.append((j-mean)/stddev)
+    for j in newdata['Label'][i]:
+        zl.append((j-mean)/stddev)
+    
     newdata.loc[i,'Z'] = z
-    
-start_date = '2021-03-15'
-end_date = '2022-05-14'
-symbol = 'BTCUSDT'
-df2 = get_data(start_date, end_date, symbol)
-
-train2 = []
-newdata = pd.DataFrame(columns=['Price','Mean','Stddev','Z'])
-
-
-for i in range(int((df2['Close'].size)/30 -1 )):
-    print(i)
-    temp = []
-    for k in range(60):
-        temp.append(int(float(df2['Close'][i*30+k])))
-    train2.append(temp)
-
-for i in range(len(train2)):
-    newdata.loc[i,'Price'] = train2[i]
-    
-meanlist = []
-for i in range(len(newdata['Price'])):
-    mean = 0
-    for j in newdata['Price'][i]:
-        mean +=j
-    mean = mean/len(newdata['Price'][0])
-    newdata.loc[i,'Mean'] = mean
-    z = []
-    stddev = 0
-    for j in newdata['Price'][i]:
-        stddev += (j-mean)**2
-    stddev = stddev/len(newdata['Price'][0])
-    newdata.loc[i,'Stddev'] = stddev
-    for j in newdata['Price'][i]:
-        z.append(int((round((j-mean)/stddev,3))*1000+1000))
-    newdata.loc[i,'Z'] = z    
+    newdata.loc[i,'ZL'] = zl
     
 newdata.to_pickle('pretrainvalid.pkl')
   
     
 #%% Finetuning 용 데이터 1~60을 주고 61~120을 예측 다음 데이터는 31~90을 주고 91~150을 예측
-start_date = '2021-06-12'
-end_date = '2022-02-01'
 
-start_date = '2022-02-02'
+start_date = '2021-02-02'
 end_date = '2022-03-02'
 symbol = 'BTCUSDT'
 ftdf = get_data(start_date, end_date, symbol)
@@ -244,74 +175,6 @@ ftpd.to_pickle('validata.pkl')
 
 ftpd.to_pickle('test2.pkl')
     
-
-#%% Norm Finetuningdata
-start_date = '2022-03-01'
-end_date = '2022-03-20'
-symbol = 'BTCUSDT'
-ftdf = get_data(start_date, end_date, symbol)
-
-ftpd = pd.DataFrame(columns=['Price','label'])
-trinput = []
-trlabel = []
-for i in range(int((ftdf['Close'].size)/30 -4 )):
-    print(i)
-    temp = []
-    temp2=[]
-    for k in range(60):
-        temp.append(int(float(ftdf['Close'][i*30+k])))
-        temp2.append(int(float(ftdf['Close'][i*30+k+60])))
-    minimum1 = min(temp)
-    minimum2 = min(temp2)
-    for l in range(len(temp)):
-        temp[l] = temp[l] - minimum1 + 100
-        temp2[l] = temp2[l] - minimum2 + 100
-        
-    trinput.append(temp)
-    trlabel.append(temp2)
-    
-
-for i in range(len(trinput)):
-    ftpd.loc[i,'Price'] = trinput[i]
-    ftpd.loc[i,'label'] = trlabel[i]
-ftpd.to_pickle('finetuning.pkl')
-
-#%% Test
-start_date = '2022-03-21'
-end_date = '2022-03-25'
-symbol = 'BTCUSDT'
-ftdf = get_data(start_date, end_date, symbol)
-
-ftpd = pd.DataFrame(columns=['Price','label','Minimum','realinput'])
-trinput = []
-trlabel = []
-minimums = []
-realinput = []
-for i in range(int((ftdf['Close'].size)/30 -4 )):
-    print(i)
-    temp = []
-    temp2=[]
-    for k in range(60):
-        temp.append(int(float(ftdf['Close'][i*30+k])))
-        temp2.append(int(float(ftdf['Close'][i*30+k+60])))
-    realinput.append(temp)
-    minimum1 = min(temp)
-    for l in range(len(temp)):
-        temp[l] = temp[l] - minimum1 + 100
-    minimums.append(minimum1)
-    trinput.append(temp)
-    trlabel.append(temp2)
-    
-
-for i in range(len(trinput)):
-    ftpd.loc[i,'Price'] = trinput[i]
-    ftpd.loc[i,'label'] = trlabel[i]
-    ftpd.loc[i,'Minimum'] = minimums[i]
-    ftpd.loc[i,'realinput'] = realinput[i]
-    
-ftpd.to_pickle('finetuning.pkl')
-ftpd.to_pickle('test2.pkl')
-
 
 
 
--- train.py
+++ train.py
@@ -1,124 +1,65 @@
 import argparse
 import os
-from glob import glob
 import dataset
 from torch.utils.data import DataLoader
-from transformers import BartConfig, BartForConditionalGeneration
 import pandas as pd
 import trainstep
 import gc
 from tqdm import tqdm
 import torch
-from transformers import get_cosine_schedule_with_warmup
 from torch.optim.lr_scheduler import LambdaLR
-from sklearn.model_selection import train_test_split
-
+import models
+import random
+import numpy as np
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+def seed_everything(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)  # type: ignore
+    torch.backends.cudnn.deterministic = True  # type: ignore
+    torch.backends.cudnn.benchmark = True  # type: ignore
+
+seed_everything(42)
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 torch.backends.cudnn.benchmark = True
-# fmt: off
+
 parser = argparse.ArgumentParser(prog="train", description="BART for price prediction")
 
 g = parser.add_argument_group("Common Parameter")
 g.add_argument("--method", type=str, choices=["default", "pretrain","finetuning"], default="default", help="training method")
 g.add_argument("--pretrained-ckpt-path", type=str, help="pretrained BART model path or name")
 g.add_argument("--batch-size", type=int, default=4, help="training batch size")
-g.add_argument("--valid-batch-size", type=int, default=256, help="validation batch size")
-g.add_argument("--epochs", type=int, default=10, help="the numnber of training epochs")
-g.add_argument("--max-learning-rate", type=float, default=2e-4, help="max learning rate")
-g.add_argument("--min-learning-rate", type=float, default=1e-5, help="min Learning rate")
-g.add_argument("--warmup-rate", type=float, default=0.05, help="warmup step rate")
-g.add_argument("--max-seq-len", type=int, default=60, help="dialogue max sequence length")
-g.add_argument("--pred-max-seq-len", type=int, default=64, help="summary max sequence length")
-g.add_argument("--all-dropout", type=float, help="override all dropout")
-g.add_argument("--logging-interval", type=int, default=100, help="logging interval")
-g.add_argument("--evaluate-interval", type=int, default=500, help="validation interval")
-g.add_argument("--masking-rate", type=float, default=0.3, help="pretrain parameter (only used with `pretrain` method)")
-
-
-class LinearWarmupLR(LambdaLR):
-    """LR Scheduling function which is increase lr on warmup steps and decrease on normal steps"""
-
-    def __init__(
-        self,
-        optimizer: torch.optim.Optimizer,
-        num_warmup_steps: int,
-        num_training_steps: int,
-        reduce_rate: float,
-        last_epoch: int = -1,
-        verbose: bool = False,
-    ) -> None:
-        """
-        Args:
-            optimizer: torch optimizer
-            num_warmup_steps: number of warmup steps
-            num_training_steps: number of whole training steps
-            reduce_rate: min Learning Rate / max Learning Rate
-        """
-        self.num_warmup_steps = num_warmup_steps
-        self.decrement = (1.0 - reduce_rate) / (num_training_steps - num_warmup_steps)
-        self.reduce_rate = reduce_rate
-
-        super().__init__(optimizer, self._get_lr, last_epoch=last_epoch, verbose=verbose)
-
-    def _get_lr(self, current_step: int) -> float:
-        if current_step < self.num_warmup_steps:
-            return current_step / self.num_warmup_steps
-        return max(1.0 - self.decrement * (current_step - self.num_warmup_steps), self.reduce_rate)
+g.add_argument("--learning-rate", type=float, default=2e-4, help="Learning rate")
+g.add_argument("--max-seq-len", type=int, default=60, help="Input Sequence's max length")
 
 def main(args: argparse.Namespace):
 
-    #os.makedirs(args.output_dir)
-
-    #if args.method == "pretrain":
-    #    train_dataset = dataset.PretrainDataset(dataframe = data ,max_seq_len=args.max_seq_len)
     if args.method == "pretrain":
-        #data = pd.read_pickle('data.pkl')
         data = pd.read_pickle('pretrain.pkl')
     elif args.method == "finetuning":
         data = pd.read_pickle('finetuning.pkl')
         eval_data = pd.read_pickle('validata.pkl')
         
     train_step = {"pretrain": trainstep.pretrain_step,"finetuning": trainstep.finetuning_step}[args.method]
-    datasets = {"pretrain": dataset.PretrainDataset,"finetuning": dataset.FinetuningDataset,
-                "normpretrain": dataset.NormPretrainDataset}[args.method]
+    datasets = {"pretrain": dataset.PretrainDataset,"finetuning": dataset.FinetuningDataset1}[args.method]
     
     train_dataset = datasets(dataframe = data ,max_seq_len=args.max_seq_len)
     train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size)
     
     eval_dataset = datasets(dataframe = eval_data ,max_seq_len=args.max_seq_len)
     eval_dataloader = DataLoader(eval_dataset, shuffle=True, batch_size=args.batch_size)
-    
-    override_args = (
-        {
-            "dropout": args.all_dropout,
-            "attention_dropout": args.all_dropout,
-            "activation_dropout": args.all_dropout,
-            "classifier_dropout": args.all_dropout,
-        }
-        if args.all_dropout
-        else {}
-    )
-    model = BartForConditionalGeneration(BartConfig.from_pretrained('default.json', **override_args)).to(device)
-    print(model.config)
-    if args.method == "finetuning":
-        #model.load_state_dict(torch.load('pretrain_90_epoch.ckpt'))
-        model.load_state_dict(torch.load('finetuning_67_epoch.ckpt'))
         
     
     epochs = args.epochs
-    learning_rate = 2e-4
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
-    """
-    total_steps = len(train_dataloader) * epochs
-    scheduler = LinearWarmupLR(optimizer,
-            int(total_steps * 0.05),
-            total_steps,
-            1e-5 / 2e-4)
-    """
+    model = models.LSTMModel()
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate) 
     
     
     for epoch in range(epochs): 
@@ -140,7 +81,7 @@ def main(args: argparse.Namespace):
                 'Loss': '{:06f}'.format(batch_loss.item()),
                 'Total Loss' : '{:06f}'.format(total_train_loss/(batch+1)),
                 'Total ACC' : '{:06f}'.format(total_train_acc/(batch+1)),
-                'learning rate' : '{:06f}'.format(optimizer.param_groups[0]['lr'])
+                'learning rawte' : '{:06f}'.format(optimizer.param_groups[0]['lr'])
             })
             
         training = False
@@ -158,15 +99,7 @@ def main(args: argparse.Namespace):
                 'Total Val ACC' : '{:06f}'.format(total_val_acc/(batch+1))
             })
         
-            
         torch.save(model.state_dict(), f'{args.method}_{epoch+1}_epoch.ckpt')
-        #scheduler.step()
-            
-    
-
-    #model_dir = os.path.join(args.output_dir, "models")
-
-
 
 if __name__ == "__main__":
     exit(main(parser.parse_args()))
